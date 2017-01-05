f4e20d38a45d960be65915372aebfcd5a63f1b8b
Hans Ellegård <psyill.net@gmail.com>
2017-01-05 20:54:46 +0100

    Removed parenthesis layer from videck. Dead keys in uncomfortable positions.

--- layouts/community/ergodox/videck/keymap.c
+++ layouts/community/ergodox/videck/keymap.c
@@ -7,40 +7,40 @@ enum {
   BASE = 0,     // Default layer
   ARROWS = 1,   // Arrows and Fx keys layer
   MOUSE = 2,    // Mouse movement and buttons layer
-  PARENS = 3,   // Parenthesis easy access layer
 
   TD_L = 0,     // Tap dance index for left shift
-  TD_R = 1      // Tap dance index for right shift
+  TD_R = 1,     // Tap dance index for right shift
+  TD_A = 2      // Tap dance index for arrow layer key
 };
 
 // See the videck.png image for a visualization of the layout.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox(
     // left hand
-    KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_HOME,
-    KC_EQL,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_END,
-    MO(PARENS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-    TD(TD_L),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC,
-    KC_LCTL,    KC_LALT, KC_VOLD, KC_VOLU, KC_MUTE,
-                                                    KC_ESC,  KC_LGUI,
-                                                             TG(MOUSE),
-                                           KC_SPC,  KC_TAB,  TG(ARROWS),
+    KC_GRV,   KC_1,    KC_2,    KC_3,  KC_4,   KC_5,   KC_NO,
+    KC_EQL,   KC_Q,    KC_W,    KC_E,  KC_R,   KC_T,   KC_NO,
+    KC_BSPC,  KC_A,    KC_S,    KC_D,  KC_F,   KC_G,
+    TD(TD_L), KC_Z,    KC_X,    KC_C,  KC_V,   KC_B,   KC_LCTL,
+    KC_MUTE,  KC_VOLD, KC_VOLU, KC_NO, KC_ESC,
+                                               KC_NO,  KC_NO,
+                                                       TG(MOUSE),
+                                       KC_SPC, KC_TAB, KC_LGUI,
     // right hand
-    KC_PGUP, KC_6,   KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-    KC_PGDN, KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
-             KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    KC_DELT, KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, TD(TD_R),
-                     KC_LBRC, KC_RBRC, KC_EQL, KC_RALT, KC_RCTL,
-    KC_APP,  KC_ESC,
+    KC_NO,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+    KC_NO,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+              KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    TD(TD_A), KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(TD_R),
+                      KC_LALT, KC_NO,   KC_LBRC, KC_RBRC, KC_DELT,
+    KC_NO,    KC_APP,
     KC_INS,
-    KC_SLCK, KC_ENT, KC_SPC
+    KC_SLCK,  KC_ENT, KC_SPC
   ),
   [ARROWS] = LAYOUT_ergodox(
     // left hand
     KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_HOME, KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                  KC_TRNS, KC_TRNS,
                                                           KC_NO,
@@ -64,32 +64,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                  KC_BTN3, KC_TRNS,
                                                           KC_TRNS,
-                                        KC_BTN1, KC_BTN2, KC_NO,
+                                        KC_BTN1, KC_BTN2, KC_TRNS,
     // right hand
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
              KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,
-    KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-  [PARENS] = LAYOUT_ergodox(
-    // left hand
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                                 KC_TRNS, KC_TRNS,
-                                                          KC_NO,
-                                        KC_TRNS, KC_TRNS, KC_NO,
-    // right hand
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS,
+    KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS,
     KC_TRNS,
@@ -106,24 +86,45 @@ typedef struct
   videck_tap_dance_trigger_t trigger;
 } videck_tap_dance_tuple_t;
 
-static void videck_tap_dance_finished (qk_tap_dance_state_t *state, void *user_data) {
-  videck_tap_dance_tuple_t *const tuple = (videck_tap_dance_tuple_t *)user_data;
+typedef struct
+{
+  uint8_t layer;
+} videck_tap_dance_layer_t;
+
+static void videck_tap_dance_finished(qk_tap_dance_state_t *state, void *user_data) {
+  videck_tap_dance_tuple_t *const tuple = (videck_tap_dance_tuple_t*)user_data;
 
   if (state->count == 1) {
-    register_code16 (tuple->kc1);
+    register_code16(tuple->kc1);
   } else if (state->count == 2) {
-    register_code16 (tuple->kc2);
+    register_code16(tuple->kc2);
   }
 }
 
-static void videck_tap_dance_reset (qk_tap_dance_state_t *state, void *user_data) {
-  videck_tap_dance_tuple_t *const tuple = (videck_tap_dance_tuple_t *)user_data;
+static void videck_tap_dance_reset(qk_tap_dance_state_t *state, void *user_data) {
+  videck_tap_dance_tuple_t *const tuple = (videck_tap_dance_tuple_t*)user_data;
 
   if (state->count == 1) {
-    unregister_code16 (tuple->kc1);
+    unregister_code16(tuple->kc1);
   } else if (state->count == 2) {
-    unregister_code16 (tuple->kc2);
+    unregister_code16(tuple->kc2);
     tuple->trigger(tuple->kc2);
+  }
+}
+
+static void videck_tap_dance_layer_finished(qk_tap_dance_state_t *state, void *user_data) {
+   const uint8_t layer = ((videck_tap_dance_layer_t*)user_data)->layer;
+
+  if (state->count == 1 || state->count == 2) {
+    layer_invert(layer);
+  }
+}
+
+static void videck_tap_dance_layer_reset(qk_tap_dance_state_t *state, void *user_data) {
+   const uint8_t layer = ((videck_tap_dance_layer_t*)user_data)->layer;
+
+  if (state->count == 1) {
+    layer_invert(layer);
   }
 }
 
@@ -139,9 +140,15 @@ static void videck_caps_trigger (const uint16_t kc) {
     .user_data = (void *)&((videck_tap_dance_tuple_t) { kc1, kc2, double_trigger }),  \
   }
 
+#define ACTION_TAP_DANCE_DOUBLE_LAYER(layer) { \
+    .fn = { NULL, videck_tap_dance_layer_finished, videck_tap_dance_layer_reset }, \
+    .user_data = (void *)&((videck_tap_dance_layer_t) { layer }),  \
+  }
+
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_L] = ACTION_TAP_DANCE_DOUBLE_TRIGGER(KC_LSFT, KC_CAPS, videck_caps_trigger),
-  [TD_R] = ACTION_TAP_DANCE_DOUBLE_TRIGGER(KC_RSFT, KC_CAPS, videck_caps_trigger)
+  [TD_R] = ACTION_TAP_DANCE_DOUBLE_TRIGGER(KC_RSFT, KC_CAPS, videck_caps_trigger),
+  [TD_A] = ACTION_TAP_DANCE_DOUBLE_LAYER(ARROWS)
 };
 
 // Runs just one time when the keyboard initializes.
--- layouts/community/ergodox/videck/readme.md
+++ layouts/community/ergodox/videck/readme.md
@@ -7,19 +7,23 @@ especially considered.
 # Features
 * Mouse movements using the Vi movement keys.
 * Vi movement keys doubling as arrow keys.
-* Shift lock using double tap.
-* A coder's modifier key for easy input of parenthesis.
-* Acess to the escape key using either thumb.
+* Mimicking the readline commands ^A and ^E to generate Home and End.
+* Mimicking the Vim commands ^F and ^B to generate Page Down and Page Up.
+* Double tapping shift key generates Caps Lock.
+* Acess to the escape key using left thumb.
 * Reasonably accessible Scroll Lock key (useful if you, like me, want to switch
   to a non-US layout once in a while in X).
+* Dead, unused keys where the fingers have a hard time reaching. Keeps me from
+  accidentaly hitting the wrong key.
 
 # Missing keys
-* Caps Lock (shift lock is used instead).
 * Pause/Break.
 * Print Screen.
+* Right Alt.
+* Right Ctrl.
 
 # LEDs
-* Shift lock is indicated using first LED.
+* Caps Lock is indicated using first LED.
 * Arrow keys layer is indicated using second LED.
 * Mouse keys layer is indicated using third LED.
 
